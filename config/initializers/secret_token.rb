199f3211f3b17f84126c2e1af9bee5466a7071ad
tmarkiewicz <tmarkiewicz@gmail.com>
2011-07-06 15:17:32 -0600

    my new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuStatsmixTest::Application.config.secret_token = 'eb07bab74d99a10ccc3e950fe8c200202442f46d11f2e987b8a13aa003521862b06cda1ddbc2d4f4bba99eac6b73e9449604358695d6d2aac2cf5b982cc9c712'
